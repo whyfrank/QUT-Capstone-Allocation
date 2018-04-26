f6ba43f6c56c380c28837fb9c9e7935dcccc2465
Steven Harse <steven.harse@connect.qut.edu.au>
2018-04-26 20:56:28 +1000

    UI size decreased, implemented static project proposal section.

---
+++ capstone/capstone/public/javascripts/staff.js
@@ -0,0 +1,35 @@
+previousProposalSelection = null;
+
+//Requests an HTML document and loads it into the proposal section.
+function getProposal(id) {
+  if (previousProposalSelection	!= null) {
+	previousProposalSelection.setAttribute("style","");
+  }
+
+  loadingDisplay("proposal-view");	
+  document.getElementById(id).style.background = "#555"
+  document.getElementById(id).style.color = "#fff"
+  previousProposalSelection = document.getElementById(id);
+	
+  if (window.XMLHttpRequest) {
+	// code for IE7+, Firefox, Chrome, Opera, Safari
+	xmlhttp=new XMLHttpRequest();
+  } else {  // code for IE6, IE5
+	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  xmlhttp.onreadystatechange=function() {
+	// If the response is ready, and OK, then display the results in the contents section.
+	if (this.readyState==4 && this.status==200) {
+		
+	  // Does nothing but show the loading screen for debugging purposes.
+	  sleep(1000);
+	  document.getElementById("proposal-view").innerHTML=this.responseText;
+	} else if (this.readyState==4 && this.status==404) {
+	  document.getElementById("proposal-view").innerHTML="<h2>This resource cannot be found: Error " + this.status + ".</h2>";
+	} else if (this.readyState==4) {
+	  document.getElementById("proposal-view").innerHTML="<h2>An error has occured: Error " + this.status + ".</h2>";
+	}
+  }
+  xmlhttp.open("GET","proposal?id=" + id,true);
+  xmlhttp.send();
+}
--- capstone/capstone/routes/index.js
+++ capstone/capstone/routes/index.js
@@ -3,12 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IT Capstone' });
+  res.render('index', { title: 'IT Capstone', page: req.query.page});
 });
 
 /* GET view projects. */
 router.get('/viewprojects', function(req, res, next) {
   res.render('viewprojects', {layout: false});
+});
+
+/* GET proposals. */
+router.get('/proposals', function(req, res, next) {
+  res.render('proposals', {layout: false});
+});
+
+/* GET proposal. */
+router.get('/proposal', function(req, res, next) {
+  res.render('proposal', {layout: false});
 });
 
 module.exports = router;
