d227b7a08d3665f46a8870d32293c7f22e139715
whyfrank <yannikfrank@outlook.com>
2018-04-30 20:59:27 +1000

    Salt hashing implementation
    Begun basic foundation of salt and node.js crypto module to hash user passwords for secure storage

--- capstone/capstone/routes/index.js
+++ capstone/capstone/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var router = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -32,3 +33,28 @@ router.get('/viewstudents', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+/* Generate random string of characters to be used as salt */
+var generateSalt = function(length){
+  return crypto.randomBytes(Math.ceil(length/2))
+    .toString('hex') /*convert to hexadecimal */
+    .slice(0,length); /* return correct amount of characters */
+};
+
+/* Hash the password with sha256 */
+var sha256 = function(password, salt){
+  var hash crypto.createHmac('sha256', salt); /* Hashing algorithm sha256 */
+  hash.update(password);
+  var value = hash.digest('hex');
+  return {
+    salt: salt,
+    passwordHash: value
+  };
+};
+
+/* Generates hashed password to be stored in DB */
+function saltHashPassword(userpassword){
+  var salt = generateSalt(16);
+  var passwordData = sha256(userpassword, salt);
+}
