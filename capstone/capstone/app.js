0bf9059d01de010c4975a6708e6dddc6a981b8e5
Steven Harse <steven.harse@connect.qut.edu.au>
2018-05-22 15:40:01 +1000

    Made minor changes for Handlebars Helpers to work.

--- capstone/capstone/app.js
+++ capstone/capstone/app.js
@@ -10,6 +10,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var hbs = require('hbs');
+var helpers = require('handlebars-helpers')();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
